0be652df75af9458f0f168d644459612599faf7f
Andy Gill <andygill@ku.edu>
2014-04-12 15:49:57 -0500

    Adding Sign index.

---
+++ src/Rubik/Sign.hs
@@ -0,0 +1,17 @@
+module Rubik.Sign where
+
+import Data.Ix
+
+import Rubik.Reverse as R
+
+
+data Sign = Minus | Plus
+    deriving (Eq,Ord,Enum,Ix)
+
+instance Show Sign where
+    show Minus = "-"
+    show Plus  = "+"
+
+instance R.Reverse Sign where
+    reverse Minus = Plus
+    reverse Plus  = Minus
