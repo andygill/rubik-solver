14fbef90f647b12b20f5ef28ea9c1bb8aa02ea97
Andy Gill <andygill@ku.edu>
2014-04-12 21:41:51 -0500

    Fixing turn into a direction.

--- src/Rubik/Turn.hs
+++ src/Rubik/Turn.hs
@@ -1,8 +1,24 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
 module Rubik.Turn where
         
+import Data.Array
 
-class Turn v o where
-  -- apply a unit turn to o, when viewed using v
-  turn :: v -> o -> o
-                  
+data Turn = NoTurn | Clock | OneEighty | CounterClock
+   deriving (Eq,Ord,Show,Enum,Ix)
+
+instance Num Turn where
+  x + y = fromInteger ((turnToInteger x + turnToInteger y) `mod` 4)
+  x - y = fromInteger ((turnToInteger x - turnToInteger y) `mod` 4)
+  x * y = fromInteger ((turnToInteger x * turnToInteger y) `mod` 4)
+  negate x = fromInteger (negate (turnToInteger x) `mod` 4)
+  abs x    = fromInteger (abs    (turnToInteger x) `mod` 4)
+  signum x = fromInteger (signum (turnToInteger x) `mod` 4)
+  
+  fromInteger 0 = NoTurn
+  fromInteger 1 = Clock
+  fromInteger 2 = OneEighty
+  fromInteger 3 = CounterClock
+
+turnToInteger NoTurn       = 0
+turnToInteger Clock        = 1
+turnToInteger OneEighty    = 2
+turnToInteger CounterClock = 3
