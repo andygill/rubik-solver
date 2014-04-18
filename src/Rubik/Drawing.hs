7067131ac1f24fba9687f1537b633b9773c9c3b3
Andy Gill <andygill@ku.edu>
2014-04-17 20:58:09 -0500

    Adding drawing

---
+++ src/Rubik/Drawing.hs
@@ -0,0 +1,40 @@
+module Rubik.Drawing where
+
+import Graphics.Blank
+
+import Rubik.Shape
+import Rubik.Map
+
+import Rubik.Face -- for test
+import Rubik.Cube -- for test
+import Rubik.Color -- for test
+import Graphics.Blank
+
+main = blankCanvas 3000 $ \ context -> do
+        send context $ do
+                drawShape 
+                  $ borderShape 0.1
+                  $ drawMap cubePlacement cube
+ where
+  cube = fmap (borderShape 0.01)
+       $ fmap face
+       $ fmap showColor
+       $ start
+
+  face col = background "#202020" 0.01 0.1
+                       $ drawMap facePlacement $ 
+                           fmap (borderShape 0.05) $ 
+                           fmap tile $
+                           mkMap (\ s -> col)
+
+
+                
+--foreground :: String -> Float -> Float -> Shape -> Shape
+
+
+----------------------------------------------------------------------------------------------------
+
+--beside :: Shape -> Shape -> Shape
+--beside g h = (x,y) -> 
+
+
