2e336423d07f9bb2a5e23959c887a760c249822a
Andy Gill <andygill@ku.edu>
2014-11-27 12:30:10 -0600

    Remove the V3 slicer

--- src/Rubik/V3.hs
+++ src/Rubik/V3.hs
@@ -8,12 +8,6 @@ import Rubik.V2
 data V3 a = V3 a a a
         deriving (Eq,Ord,Show)
 
-data V3slice a 
-   = V3X a (V2 a)
-   | V3Y a (V2 a)
-   | V3Z a (V2 a)
-
-
 --instance Reverse a => Rotate (V2 a) where
 --  turn (V2 x y) = (V2 y (R.reverse x))
 
