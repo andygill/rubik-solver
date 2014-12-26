fc4ae733df62ef74c42673201fcfdd3bcf91a611
Andy Gill <andygill@ku.edu>
2014-12-25 22:20:36 -0600

    Fixing Y rotations.

--- src/Rubik/V3.hs
+++ src/Rubik/V3.hs
@@ -20,7 +20,7 @@ instance Negate Turn3D where
 
 instance Negate a => Rotate Turn3D (V3 a) where
   rotate (Turn3D t X) (V3 x y z) = V3 x  y' z' where (V2 y' z') = rotate t $ V2 y z
-  rotate (Turn3D t Y) (V3 x y z) = V3 x' y  z' where (V2 x' z') = rotate t $ V2 x z
+  rotate (Turn3D t Y) (V3 x y z) = V3 x' y  z' where (V2 z' x') = rotate t $ V2 z x -- intentually flipped
   rotate (Turn3D t Z) (V3 x y z) = V3 x' y' z  where (V2 x' y') = rotate t $ V2 x y
 
 instance Key a => Key (V3 a) where
